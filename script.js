c825093c303ea4f7ea5d7012a6675a88f3c5e2f9
youssefwargui <youssefwargui100@gmail.com>
2026-01-18 10:13:13 +0100

    ex

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+// 1 — Corriger les déclarations de variables
 
+var animal = 'monkey'; 
+var monkey = 'animal'; 
+var x= 15; 
+var y =10; 
+var str = 'y3ni?'; 
+var bln = false; 
+var isTenEven = 10 % 2 ;
+
+// 2 — Créer et combiner des variables
+
+var firstName = "youssef"
+
+var lastName = "wargui"
+
+var fullName = firstName + " " + lastName
+
+//3 — Fonction compteur
+
+var count = 0 
+function counter() {
+  count += 1;        // avec l'aide de chatGPT
+  return count;
+}
+counter()  
+
+//4 — Déclaration et utilisation des variables
+
+var monAge = 26
+// Que se passe-t-il si on utilise une variable non déclarée ?
+// undefined
+
+// 5 — Types de variables
+
+var nom = "youssef"
+(typeof nom)
+var bln = true
+(typeof bln)
+var nombre = 10
+(typeof nombre)
+
+// 6 — Calcul simple
+
+function sumTwoNumbers(n1 , n2) {
+    var result = n1 + n2
+    return result
+}
+sumTwoNumbers()
+
+// 7 — Calcul du prix avec taxe
+
+function priceWithTax(price , tax) {
+  var taxAmount = price * tax
+  var total =  price + taxAmount
+   return total
+}
+priceWithTax()
